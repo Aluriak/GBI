32bf6280310d40f8b266203bc59fe4093c5c19f6
Aluriak <lucas.bourneuf@laposte.net>
2015-11-27 00:06:19 +0100

    hypergeometric test is ok now

--- hypergeometrictest.py
+++ hypergeometrictest.py
@@ -12,11 +12,16 @@ def phyper(pop_size, pop_condition_count,
     subset_size: number of element in subset
     subset_condition_count: number of element with condition in subset
 
-    source: https://www.biostars.org/p/66729/
+    A loss of precision is expected for big values.
+
+    sources:
+        - https://www.biostars.org/p/66729/
+        - http://stackoverflow.com/questions/6594840/what-are-equivalents-to-rs-phyper-function-in-python
+        - http://stackoverflow.com/questions/32859103/python-scipys-hypergeometric-test-not-equal-to-r-or-sas
     Other interesting links about hypergeometric tests: 404 not found
 
     """
-    return stats.hypergeom.sf(subset_condition_count - 1,
+    return stats.hypergeom.sf(subset_condition_count - 1,  # without -1, results are generally false
                               pop_size, pop_condition_count, subset_size)
 
 if __name__ == '__main__':
